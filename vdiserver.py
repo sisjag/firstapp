976043fcf1616556045cfa1009fce8d80577c909
Sisjag <sisjag@gmail.com>
2018-05-05 08:03:17 +0200

    primera subida

---
+++ vdiserver.py
@@ -0,0 +1,21 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/vdi1')
+def vdi1():
+	f = open("vd1.txt")
+	devuelto =f.read()
+	f.close()
+	return devuelto
+@app.route('/vdi2')
+def vdi2():
+
+	f = open("vd2.txt")
+	devuelto =f.read()
+	f.close()
+	return devuelto
+
+if __name__== '__main__':
+	app.run(debug = True,host='192.168.1.233',port= 8001)
+
